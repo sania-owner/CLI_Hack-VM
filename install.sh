5e1a429a690b4746ff1fa6cf020202267beba9aa
sania-owner <saniaowner@ya.ru>
2022-04-08 01:48:50 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
-sudo git clone https://github.com/sania-owner/CLI_Hack-VM.git "$HOME"/DDOS
+sudo git clone https://github.com/sania-owner/CLI_Hack-VM.git /root/DDOS
 sudo apt -y install  procps kmod iputils-ping wget php-cli php-mbstring openvpn
-ln -s /media "$HOME"/DDOS
-sudo chmod -R 0777 /media; sudo chmod -R 0777 "$HOME"/DDOS
+ln -s /media /root/DDOS
+sudo chmod -R 0777 /media; sudo chmod -R 0777 /root"/DDOS
 rm /root/install.sh
