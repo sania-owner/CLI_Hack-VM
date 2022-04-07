9381af47866400f7a565ef93f4a83d516ff59574
sania-owner <saniaowner@ya.ru>
2022-04-08 01:43:49 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-/root/DDOS/stop.sh
-
+cd /root/DDOS; ./stop.sh
 rm -rf /root/DDOS
 cd /root; wget https://raw.githubusercontent.com/sania-owner/CLI_Hack-VM/main/install.sh; sudo chmod ugo+x install.sh; sh ./install.sh
 rm /root/update.sh
